849a9475c51fd8e3e9140e6baae27fc8a16a9f62
Renata Carreira <re.carreira@gmail.com>
2013-11-05 09:37:34 -0200

    static settings

--- naparede/naparede/urls.py
+++ naparede/naparede/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from settings import MEDIA_ROOT
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,4 +18,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

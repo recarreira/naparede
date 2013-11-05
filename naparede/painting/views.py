a37386166a1feaffe947a2e474e64c5978a014ba
Renata Carreira <re.carreira@gmail.com>
2013-11-05 09:55:48 -0200

    home: using jquery chaos for paintings

--- naparede/painting/views.py
+++ naparede/painting/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render_to_response
 from models import Painting
-
+from models import Galery
 
 def home(request):
     paintings = Painting.objects.all()
+    galeries = Galery.objects.all()
     return render_to_response('index.html', {
         "paintings": paintings,
+        "galeries": galeries,
         })
